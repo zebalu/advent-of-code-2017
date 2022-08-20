ada2af70a054538cf55e99e44dc311bb668f8c08
Balazs Zaicsek <zebalu@gmail.com>
2022-08-20 19:02:45 +0200

    5 times speedup on day 15

--- aoc2017/src/main/groovy/io/github/zebalu/aoc2017/Day15.groovy
+++ aoc2017/src/main/groovy/io/github/zebalu/aoc2017/Day15.groovy
@@ -1,17 +1,8 @@
 package io.github.zebalu.aoc2017
 
 import groovy.transform.CompileDynamic
-
-import java.time.Duration
-import java.time.Instant
-import java.util.function.IntFunction
-import java.util.function.LongPredicate
-import java.util.function.Predicate
 import java.util.regex.Pattern
-
-import groovy.transform.Canonical
 import groovy.transform.CompileStatic
-import groovy.transform.MapConstructor
 
 @CompileStatic
 class Day15 extends AbstractDay {
@@ -37,12 +28,13 @@ class Day15 extends AbstractDay {
     @CompileStatic
     @Override
     protected void solve1() {
-        def aa = new AcceptAll()
-        def generatorA = new RandomGenerator(aStart, A_MUL, DIV, aa).iterator()
-        def generatorB = new RandomGenerator(bStart, B_MUL, DIV, aa).iterator()
+        long a = aStart
+        long b = bStart
         int counter = 0
-        40_000_000.times {
-            if ((generatorA.next() & 0xffff) == (generatorB.next() & 0xffff)) {
+        for(int i=0; i<40_000_000; ++i) {
+            a = (a * A_MUL) % DIV
+            b = (b * B_MUL) % DIV
+            if ((a & 0xffff) == (b & 0xffff)) {
                 ++counter
             }
         }
@@ -52,77 +44,22 @@ class Day15 extends AbstractDay {
     @CompileStatic
     @Override
     protected void solve2() {
-        def generatorA = new RandomGenerator(aStart, A_MUL, DIV, new DividableBy4()).iterator()
-        def generatorB = new RandomGenerator(bStart, B_MUL, DIV, new DividableBy8()).iterator()
+        long a = aStart
+        long b = bStart
         int counter = 0
-        5_000_000.times {
-            if ((generatorA.next() & 0xffff) == (generatorB.next() & 0xffff)) {
+        for(int i=0; i<5_000_000; ++i) {
+            a = (a * A_MUL) % DIV
+            while(( a & 3) != 0) {
+                a = (a * A_MUL) % DIV
+            }
+            b = (b * B_MUL) % DIV
+            while((b & 7) != 0) {
+                b = (b * B_MUL) % DIV
+            }
+            if ((a & 0xffff) == (b & 0xffff)) {
                 ++counter
             }
         }
         solution2 = counter
-    }
-
-    @Canonical
-    @MapConstructor(includeFields = true)
-    @CompileStatic
-    private static final class RandomGenerator implements Iterable<Integer> {
-        private final int start
-        private final int mul
-        private final int div
-        private final LongPredicate filter
-
-        RandomGenerator(int start, int mul, int div, LongPredicate filter) {
-            this.start = start;
-            this.mul = mul;
-            this.div = div;
-            this.filter = filter;
-        }
-
-        @Override
-        Iterator<Integer> iterator() {
-            long[] num = new long[]{(long) start};
-            return new Iterator<Integer>() {
-                @Override
-                boolean hasNext() {
-                    return true;
-                }
-
-                @Override
-                Integer next() {
-                    num[0] = (num[0] * mul) % div;
-                    while (!filter.test(num[0])) {
-                        num[0] = (num[0] * mul) % div;
-                    }
-                    return (int) num[0];
-                }
-            };
-        }
-    }
-
-    private static final class AcceptAll implements LongPredicate {
-
-        @Override
-        boolean test(long value) {
-            return true
-        }
-    }
-
-    private static final class DividableBy4 implements LongPredicate {
-
-        @Override
-        boolean test(long value) {
-            return (value & 3) == 0
-        }
-
-    }
-
-    private static final class DividableBy8 implements LongPredicate {
-
-        @Override
-        boolean test(long value) {
-            return (value & 7) == 0
-        }
-
     }
 }
